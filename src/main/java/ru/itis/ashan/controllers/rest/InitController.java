41f5fd3f00fe92c33b863992cd6c7fb69b331856
saydar31 <saidar31@yandex.ru>
2020-07-20 17:39:40 +0300

    fix employer

---
+++ src/main/java/ru/itis/ashan/controllers/rest/InitController.java
@@ -0,0 +1,37 @@
+package ru.itis.ashan.controllers.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itis.ashan.entities.admin.Admin;
+import ru.itis.ashan.entities.user.Role;
+import ru.itis.ashan.entities.user.State;
+import ru.itis.ashan.repositories.UserRepository;
+
+@RestController
+public class InitController {
+    private boolean state = true;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/initAdmin")
+    @ResponseBody
+    public String init() {
+        if (state) {
+            Admin admin = Admin.builder()
+                    .mail("alex@mail.ru")
+                    .emailIsConfirmed(true)
+                    .hashPassword(passwordEncoder.encode("1"))
+                    .state(State.CONFIRMED)
+                    .role(Role.ADMIN)
+                    .build();
+            userRepository.save(admin);
+            state = false;
+        }
+        return "admin initialized";
+    }
+}
