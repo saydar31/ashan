2064a0dc42349380d0568b13e993febf5c791458
saydar31 <saidar31@yandex.ru>
2020-07-22 00:14:51 +0300

    fix accept, refuse buttons

--- src/main/java/ru/itis/ashan/controllers/rest/CompetenceRestController.java
+++ src/main/java/ru/itis/ashan/controllers/rest/CompetenceRestController.java
@@ -3,6 +3,7 @@ package ru.itis.ashan.controllers.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import ru.itis.ashan.entities.teacher.Teacher;
 import ru.itis.ashan.entities.user.UserModel;
 import ru.itis.ashan.security.details.UserDetailsImpl;
+import ru.itis.ashan.services.AuthenticationService;
 import ru.itis.ashan.services.CompetenceConfirmationService;
 
 @Controller
@@ -18,10 +20,13 @@ public class CompetenceRestController {
     @Autowired
     private CompetenceConfirmationService competenceConfirmationService;
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @PostMapping("/competence/student/{id:\\d+}/confirm")
     @PreAuthorize("hasAnyAuthority('TEACHER')")
-    public ResponseEntity<?> confirm(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
-        UserModel user = userDetails.getUser();
+    public ResponseEntity<?> confirm(Authentication authentication, @PathVariable Long id) {
+        UserModel user = authenticationService.getUserModel(authentication);
         Teacher teacher = (Teacher) user;
         competenceConfirmationService.confirm(id, teacher);
         return ResponseEntity.ok().build();
@@ -29,8 +34,8 @@ public class CompetenceRestController {
 
     @PostMapping("/competence/student/{id:\\d+}/refuse")
     @PreAuthorize("hasAnyAuthority('TEACHER')")
-    public ResponseEntity<?> decline(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable("id") Long id) {
-        UserModel user = userDetails.getUser();
+    public ResponseEntity<?> decline(Authentication authentication, @PathVariable("id") Long id) {
+        UserModel user = authenticationService.getUserModel(authentication);
         Teacher teacher = (Teacher) user;
         competenceConfirmationService.refuse(id, teacher);
         return ResponseEntity.ok().build();
--- src/main/java/ru/itis/ashan/controllers/rest/InterviewRestController.java
+++ src/main/java/ru/itis/ashan/controllers/rest/InterviewRestController.java
@@ -3,12 +3,14 @@ package ru.itis.ashan.controllers.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itis.ashan.entities.employer.Employer;
 import ru.itis.ashan.security.details.UserDetailsImpl;
+import ru.itis.ashan.services.AuthenticationService;
 import ru.itis.ashan.services.InterviewResultService;
 
 @RestController
@@ -16,17 +18,20 @@ public class InterviewRestController {
     @Autowired
     private InterviewResultService interviewResultService;
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @PostMapping("/interview/student/{id:\\d+}/refuse")
     @PreAuthorize("hasAuthority('EMPLOYER')")
-    public ResponseEntity<?> refuse(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        interviewResultService.refuse(id, (Employer) userDetails.getUser());
+    public ResponseEntity<?> refuse(@PathVariable Long id, Authentication authentication) {
+        interviewResultService.refuse(id, (Employer) authenticationService.getUserModel(authentication));
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/interview/student/{id:\\d+}/accept")
     @PreAuthorize("hasAuthority('EMPLOYER')")
-    public ResponseEntity<?> accept(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        interviewResultService.accept(id, (Employer) userDetails.getUser());
+    public ResponseEntity<?> accept(@PathVariable Long id, Authentication authentication) {
+        interviewResultService.accept(id, (Employer) authenticationService.getUserModel(authentication));
         return ResponseEntity.ok().build();
     }
 }
