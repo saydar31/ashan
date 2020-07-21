875c8f9c814534acfd4d4052a3778f7c36dcf86d
saydar31 <saidar31@yandex.ru>
2020-07-22 01:08:23 +0300

    fix invite

--- src/main/java/ru/itis/ashan/controllers/rest/StudentInviteController.java
+++ src/main/java/ru/itis/ashan/controllers/rest/StudentInviteController.java
@@ -3,12 +3,12 @@ package ru.itis.ashan.controllers.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itis.ashan.entities.employer.Employer;
-import ru.itis.ashan.security.details.UserDetailsImpl;
+import ru.itis.ashan.services.AuthenticationService;
 import ru.itis.ashan.services.JobInterviewService;
 
 @RestController
@@ -16,10 +16,13 @@ public class StudentInviteController {
     @Autowired
     private JobInterviewService jobInterviewService;
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @PostMapping("/student/{id:\\d+}/invite")
     @PreAuthorize("hasAuthority('EMPLOYER')")
-    public ResponseEntity<?> invite(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
-        Employer employer = (Employer) userDetails.getUser();
+    public ResponseEntity<?> invite(Authentication authentication, @PathVariable Long id) {
+        Employer employer = (Employer) authenticationService.getUserModel(authentication);
         jobInterviewService.invite(id, employer);
         return ResponseEntity.ok().build();
     }
