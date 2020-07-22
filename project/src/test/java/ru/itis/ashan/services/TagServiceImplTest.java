268a9f77be6b14f7fe15bcc65f5db81d35ea24d0
saydar31 <saidar31@yandex.ru>
2020-07-22 14:00:08 +0300

    add tests

--- project/src/test/java/ru/itis/ashan/services/TagServiceImplTest.java
+++ project/src/test/java/ru/itis/ashan/services/TagServiceImplTest.java
@@ -2,18 +2,16 @@ package ru.itis.ashan.services;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.omg.CORBA.portable.ValueOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.context.WebApplicationContext;
-import ru.itis.ashan.entities.tag.Tag;
 import ru.itis.ashan.entities.tag.TagDto;
-import ru.itis.ashan.entities.teacher.Teacher;
-import ru.itis.ashan.entities.user.Role;
-import ru.itis.ashan.entities.user.State;
 import ru.itis.ashan.repositories.UserRepository;
-import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class TagServiceImplTest {
